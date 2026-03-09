cbdcc4337812dd4af1f94347493923cef8bbee55
tenbite-daniel <tenbitedaniel60@gmail.com>
2026-03-09 12:44:04 +0300

    feat: Initial project setup
    - Configured styling (Tailwind/NativeWind)
    - Implemented custom routing and navigation
    - Customized app title and favicon

---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MovieDetails = () => {
+	return (
+		<View>
+			<Text>Movie Details</Text>
+		</View>
+	);
+};
+
+export default MovieDetails;
+
+const styles = StyleSheet.create({});
