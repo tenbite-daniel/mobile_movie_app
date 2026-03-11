e32549727cba6f71b06a2cfdc4834dbd20b8f334
tenbite-daniel <tenbitedaniel60@gmail.com>
2026-03-11 14:15:43 +0300

    feat: Added Movie Details Page
    - added new function to fetch movie details from tmdb
    - displayed the fetched information in the movie details page

--- services/api.ts
+++ services/api.ts
@@ -25,3 +25,26 @@ export const fetchMovies = async ({ query }: { query: string }) => {
 
 	return data.results;
 };
+
+export const fetchMovieDetails = async (
+	movieId: string,
+): Promise<MovieDetails> => {
+	try {
+		const response = await fetch(
+			`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KET}`,
+			{
+				method: "GET",
+				headers: TMDB_CONFIG.headers,
+			},
+		);
+
+		if (!response.ok) throw new Error("Failed to fetch movie details");
+
+		const data = response.json();
+
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
