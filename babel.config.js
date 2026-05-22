5afa3613cdc345699f0a51444d161effed2ba3f3
tenbite-daniel <tenbitedaniel60@gmail.com>
2026-05-21 23:28:04 -0700

    fix: strip dynamic import(variable) to fix Hermes build failure

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,35 @@
 module.exports = function (api) {
-	api.cache(true);
-	return {
-		presets: [
-			["babel-preset-expo", { jsxImportSource: "nativewind" }],
-			"nativewind/babel",
-		],
-	};
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      // Strip dynamic import(variable) calls that Hermes can't handle
+      function removeDynamicImports() {
+        return {
+          visitor: {
+            CallExpression(path) {
+              if (
+                path.node.callee.type === "Import" &&
+                path.node.arguments.length > 0 &&
+                path.node.arguments[0].type !== "StringLiteral"
+              ) {
+                path.replaceWith(
+                  require("@babel/types").callExpression(
+                    require("@babel/types").memberExpression(
+                      require("@babel/types").identifier("Promise"),
+                      require("@babel/types").identifier("resolve")
+                    ),
+                    [require("@babel/types").objectExpression([])]
+                  )
+                );
+              }
+            },
+          },
+        };
+      },
+    ],
+  };
 };
