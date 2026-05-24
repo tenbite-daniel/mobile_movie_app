523cb9107ff97933821279096d85de092507b16c
tenbite-daniel <tenbitedaniel60@gmail.com>
2026-05-24 14:43:56 +0300

    feat: integrate SelectionProvider into  root layout and register new stack screens
    - Wrap app in SelectionProvider
    - Register bulk-add and playlists/[id] stack screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,27 @@
 import { AuthProvider } from "@/context/AuthContext";
+import { SelectionProvider } from "@/context/SelectionContext";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function RootLayout() {
 	return (
-		<AuthProvider>
-			<StatusBar hidden={true} />
-			<Stack screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="(tabs)" />
-				<Stack.Screen name="(auth)" />
-				<Stack.Screen name="movies/[id]" />
-				<Stack.Screen name="tv/[id]" />
-				<Stack.Screen name="anime/[id]" />
-				<Stack.Screen name="watchlist" />
-			</Stack>
-		</AuthProvider>
+		<SafeAreaProvider>
+			<AuthProvider>
+				<SelectionProvider>
+					<StatusBar hidden={true} />
+					<Stack screenOptions={{ headerShown: false }}>
+						<Stack.Screen name="(tabs)" />
+						<Stack.Screen name="(auth)" />
+						<Stack.Screen name="movies/[id]" />
+						<Stack.Screen name="tv/[id]" />
+						<Stack.Screen name="anime/[id]" />
+						<Stack.Screen name="watchlist" />
+						<Stack.Screen name="bulk-add" />
+					</Stack>
+				</SelectionProvider>
+			</AuthProvider>
+		</SafeAreaProvider>
 	);
 }
